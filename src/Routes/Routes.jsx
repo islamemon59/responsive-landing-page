30c3e71c7ed9d70b48bd49da610d636ae73f345d
islamemon59 <islamemon59@gmail.com>
2025-07-28 09:55:35 +0600

    home page router setup added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,1 +1,16 @@
-export const router = 
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layout/MainLayout/MainLayout";
+import Home from "../Pages/Home/Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
